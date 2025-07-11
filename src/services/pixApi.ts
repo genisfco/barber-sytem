6cfed01ca4b4c4c72900040a4783c241dfc5d803
GenisCarvalho <genis_carvalho@hotmail.com>
2025-07-11 03:40:55 -0300

    ajustes

--- src/services/pixApi.ts
+++ src/services/pixApi.ts
@@ -1,5 +1,10 @@
 export async function gerarPixQrCode({ amount, description, payer }) {
-  const response = await fetch(`/api/pagamento/pix`, {
+
+  //DESENVOLVIMENTO - RODAR LOCALMENTE o backend - npm start
+  const response = await fetch(`http://localhost:3001/api/pagamento/pix`, {
+
+  //PRODUÇÃO - RODAR NO SERVIDOR da VERCEL
+  //const response = await fetch(`/api/pagamento/pix`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ amount, description, payer }),
